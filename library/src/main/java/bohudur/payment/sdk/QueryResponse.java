4f37c79bea766411eb0a06292e4ed46e0c0f1f34
Bohudur <services@bohudur.one>
2025-09-02 10:17:38 +0600

    Update and rename VerifyResponse.java to QueryResponse.java

--- library/src/main/java/bohudur/payment/sdk/QueryResponse.java
+++ library/src/main/java/bohudur/payment/sdk/QueryResponse.java
@@ -1,6 +1,6 @@
 package bohudur.payment.sdk;
 
 public interface VerifyResponse {
-	void onPaymentVerified(SuccessResponse response);
+	void onPaymentFound(SuccessResponse response);
     void onPaymentError(FailureResponse error);
 }
